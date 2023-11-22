583273dc9a6f8902c23f2746b59b0373b64a5f6d
beh solo kone <konesolo53@gmail.com>
2023-11-22 12:28:03 +0200

    Added missing exercises.

---
+++ user/week01/exercises/day05/javascript-first-steps/rock-paper-scissors.js
@@ -0,0 +1,14 @@
+Math.floor( Math.random() * 3 )
+function getRandomMove(){
+    return Math.floor(Math.random()* 3 );
+}
+console.log(getRandomMove(3));
+// Expected output: 0, 1 or 2
+console.log(getRandomMov(3));
+// Expected output: 0
+console.log(Math.random());
+// Expected output: a number from 0 to <1
+function checkMove( move, computerMove ){
+
+}
+
---
+++ user/week01/exercises/day05/javascript-first-steps/simple-quiz.js
@@ -0,0 +1,5 @@
+// Simple Quiz >>
+  // << simple-quiz.js
+  let name = A 
+  
+  // Correct! Not only did he create JS, the prototype of the language was ready in 10 days!
